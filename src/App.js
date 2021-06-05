c558b20e5e0997a50c63210846443b2d1e19990d
taneuzin <taneuzin@u.nus.edu>
2021-06-05 12:12:21 +0800

    Implemented SortDropdown.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import SortDropdown from "./SortDropdown";
+
 function App() {
   const [articles, setArticles] = useState([]);
   const [subreddit, setSubreddit] = useState("dogs");
@@ -11,6 +22,23 @@ function App() {
         <h1>Reddit Simulator</h1>
         <input type="text" vallue="subreddit" placeholder="subreddit" />
       </nav>
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <SortDropdown />
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+
       <div className="articles">
         {articles.map((a) => (
           <div className="post-preview" key={a.id}>
