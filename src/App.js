8bb7342f1b218bde4d49ad58c46deef583fbe307
taneuzin <taneuzin@u.nus.edu>
2021-06-06 01:18:49 +0800

    Implemented sorting Tabs for sorting by hot, new and top

--- src/App.js
+++ src/App.js
@@ -1,68 +1,70 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Nav,
-  Navbar,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-  InputGroup,
-} from "react-bootstrap";
-import SortDropdown from "./SortDropdown";
-import SubredditSearch from "./SubredditSearch";
+import Navbars from "./components/Navbars";
+import SortTabs from "./components/SortTabs";
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossorigin="anonymous"
+/>
 
 function App() {
-  const [articles, setArticles] = useState([]);
+  const [hotArticles, setHotArticles] = useState([]);
+  const [newArticles, setNewArticles] = useState([]);
+  const [topArticles, setTopArticles] = useState([]);
   const [subreddit, setSubreddit] = useState("dogs");
+
+
+//Fetch articles sorted by hot, new and top from the chosen subreddit
+  useEffect(() => {
+    fetch(`https://www.reddit.com/r/${subreddit}/hot/.json?limit=20`).then((res) => {
+      if (res.status !== 200) {
+        console.log("ERROR unable to retrieve default subreddit r/dogs");
+        return;
+      }
+
+      res.json().then((data) => {
+        if (data != null) {
+          setHotArticles(data.data.children);
+        }
+      });
+    });
+
+    fetch(`https://www.reddit.com/r/${subreddit}/new/.json?limit=20`).then((res) => {
+      if (res.status !== 200) {
+        console.log("ERROR unable to retrieve default subreddit r/dogs");
+        return;
+      }
+
+      res.json().then((data) => {
+        if (data != null) {
+          setNewArticles(data.data.children);
+        }
+      });
+    });
+
+    fetch(`https://www.reddit.com/r/${subreddit}/top/.json?limit=20`).then((res) => {
+      if (res.status !== 200) {
+        console.log("ERROR unable to retrieve default subreddit r/dogs");
+        return;
+      }
+
+      res.json().then((data) => {
+        if (data != null) {
+          setTopArticles(data.data.children);
+        }
+      });
+    })
+  },[subreddit]);
 
   return (
     <div className="App">
-      <nav className="navbar">
-        <h1>Reddit Simulator</h1>
-      </nav>
-
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">
-          <img src="reddit-logo.png" alt="reddit logo" width="30" height="30" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <SortDropdown />
-          </Nav>
-          <SubredditSearch />
-        </Navbar.Collapse>
-      </Navbar>
-
-      <div className="articles">
-        {articles.map((a) => (
-          <div className="post-preview" key={a.id}>
-            <h2>{a.title}</h2>
-          </div>
-        ))}
-      </div>
-
-      <script
-        src="https://unpkg.com/react/umd/react.production.min.js"
-        crossorigin
-      ></script>
-
-      <script
-        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-        crossorigin
-      ></script>
-
-      <script
-        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-        crossorigin
-      ></script>
-
-      <script>var Alert = ReactBootstrap.Alert;</script>
+      <Navbars />
+      <SortTabs hotArticles={hotArticles} newArticles={newArticles} topArticles={topArticles} />
     </div>
   );
 }
