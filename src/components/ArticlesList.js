b23463adba1746f576182b754aea0d21af82a5d8
taneuzin <taneuzin@u.nus.edu>
2021-06-07 03:42:53 +0800

    improve layout of card view. change number of cards per row when screen size shrinks

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,32 +1,50 @@
 import { Row, Button, Container, Col } from "react-bootstrap";
-import { useState } from 'react' 
+import { useState } from "react";
 
 const ArticlesList = (prop) => {
   const myArticles = prop.articles;
   const view =
     prop.view === 1 ? "myCard" : prop.view === 2 ? "classic" : "compact";
-  const [score, setScore] = useState(0);
 
   return (
     <div className="article-list">
-      <Row>
-        {myArticles.map((a) => (
-          <div className={`article-preview, ${view}`} key={a.data.id}>
-            <a href={"https://reddit.com" + a.data.permalink}>
-              <div>
-                <p className="title">
-                  {view == 'myCard' ? a.data.title.substring(0, 100) +
-                    (a.data.title.length >= 100 ? "..." : "") : a.data.title}
-                </p>
-                {view !== 'compact' && <p className="author">{`${a.data.author}`}</p>}
-                {view !== 'compact' && <p className="commentCount">comments: {a.data.num_comments}</p>}
-              </div>
-            </a>
-            {view !== 'compact' &&<button>upvote</button>}
-            {view !== 'compact' &&<p className="scoreCount">{a.data.score}</p>}
-            {view !== 'compact' &&<button>downvote</button>}
-          </div>
-        ))}
+      <Row className="myRow">
+        {myArticles.map((a) => {
+          const score = a.data.score;
+          return <Col md="4">
+            <div className={`article-preview, ${view}`} key={a.data.id}>
+              <Row>
+                <a href={"https://reddit.com" + a.data.permalink}>
+                  <p className="title">
+                    {view == "myCard"
+                      ? a.data.title.substring(0, 100) +
+                        (a.data.title.length >= 100 ? "..." : "")
+                      : a.data.title}
+                  </p>
+                </a>
+              </Row>
+              <Row className="author">
+                {view !== "compact" && (
+                  <p className="author">{`${a.data.author}`}</p>
+                )}
+              </Row>
+              <Row className="comments">
+                {view !== "compact" && (
+                  <p className="commentCount">
+                    comments: {a.data.num_comments}
+                  </p>
+                )}
+                </Row>
+              <Row className="scoreRow">
+                {view !== "compact" && <button>upvote</button>}
+                {view !== "compact" && (
+                  <p className="scoreCount">{score}</p>
+                )}
+                {view !== "compact" && <button>downvote</button>}
+              </Row>
+            </div>
+          </Col>
+        })}
       </Row>
     </div>
   );
