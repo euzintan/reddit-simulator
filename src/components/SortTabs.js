25b13af72624a2c39baf09d5b38fb69bf25cd9eb
taneuzin <taneuzin@u.nus.edu>
2021-06-06 15:16:50 +0800

    saved

--- src/components/SortTabs.js
+++ src/components/SortTabs.js
@@ -2,20 +2,24 @@ import { Tabs, Tab } from "react-bootstrap";
 import ArticlesList from "./ArticlesList";
 
 const SortTabs = (prop) => {
-    const hotArticles = prop.hotArticles;
-    const newArticles = prop.newArticles;
-    const topArticles = prop.topArticles;
-    const view = prop.view;
+  const hotArticles = prop.hotArticles;
+  const newArticles = prop.newArticles;
+  const topArticles = prop.topArticles;
+  const view = prop.view;
   return (
-    <Tabs className={`tabs`}  defaultActiveKey="profile" id="uncontrolled-tab-example">
+    <Tabs
+      className={`tabs`}
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+    >
       <Tab eventKey="hot" title="Hot">
-        <ArticlesList articles={hotArticles} view={view}/>
+        <ArticlesList articles={hotArticles} view={view} />
       </Tab>
       <Tab eventKey="new" title="New">
-        <ArticlesList articles={newArticles} view={view}/>
+        <ArticlesList articles={newArticles} view={view} />
       </Tab>
       <Tab eventKey="top" title="Top">
-        <ArticlesList articles={topArticles} view={view}/>
+        <ArticlesList articles={topArticles} view={view} />
       </Tab>
     </Tabs>
   );
